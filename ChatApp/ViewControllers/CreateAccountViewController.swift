1ed6c5f52c3098d269c81e182651f75d23f47e6f
Max Gabriel <maxim.don.mg@gmail.com>
2025-12-02 18:26:06 -0500

    integrated responsive UI for keyboard appearance

--- ChatApp/ViewControllers/CreateAccountViewController.swift
+++ ChatApp/ViewControllers/CreateAccountViewController.swift
@@ -16,7 +16,9 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
-
+    var activeTextField: UITextField?
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         containerView.clipsToBounds = true
@@ -42,12 +44,64 @@ class CreateAccountViewController: UIViewController {
       
       signinAccountTextView.attributedText = attributedString
       signinAccountTextView.isEditable = false//to prevent text inside the textView to be scrollable to prevent UX conflicts
+      usernameTextField.layer.masksToBounds = true
+      emailTextField.layer.masksToBounds = true
+      passwordTextField.layer.masksToBounds = true
+      usernameTextField.delegate = self
+      emailTextField.delegate = self
+      passwordTextField.delegate = self
+      
+      let backgroundTap = UITapGestureRecognizer(target: self, action: #selector(dismisKeyboard))
+      view.addGestureRecognizer(backgroundTap)//adding the tap recogniser to entire view
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         containerView.layer.cornerRadius = 20
+      usernameTextField.layer.cornerRadius = 10
+      emailTextField.layer.cornerRadius = 10
+      passwordTextField.layer.cornerRadius = 10
     }
+  
+  
+override func viewWillAppear(_ animated: Bool) {
+  super.viewWillAppear(animated)
+  registerKeyboardNotifications()
+}
+
+override func viewWillDisappear(_ animated: Bool) {
+  super.viewWillDisappear(animated)
+  removeKeyboardNotifications() //we need to make sure to remove the observers when view is closed otherwise memory will be taken by this observers throughout the entire app!!!
+}
+
+
+func registerKeyboardNotifications() {
+  NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification: )), name: UIWindow.keyboardWillShowNotification, object: nil)
+  NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification: )), name: UIWindow.keyboardWillHideNotification, object: nil)
+}
+
+func removeKeyboardNotifications() {
+  NotificationCenter.default.removeObserver(self, name: UIWindow.keyboardWillShowNotification, object: nil)
+  NotificationCenter.default.removeObserver(self, name: UIWindow.keyboardWillHideNotification, object: nil)
+}
+
+@objc func keyboardWillShow(notification: NSNotification) {
+  print("keyboard shown")
+  guard let keyboardSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+    else { return }
+  let keyboardOffset = view.convert(keyboardSize.cgRectValue, from: nil).size.height // assigning the keyboard frame
+  let totalOffset = activeTextField == nil ? keyboardOffset : keyboardOffset + activeTextField!.frame.height
+  scrollView.contentInset.bottom = totalOffset
+}
+
+@objc func keyboardWillHide(notification: NSNotification) {
+  print("keyboard hidden")
+  scrollView.contentInset.bottom = 0
+}
+
+@objc func dismisKeyboard() {
+  view.endEditing(true)
+}
     
     @IBAction func createAccountButtonTapped(_ sender: Any) {
     }
@@ -63,5 +117,23 @@ extension CreateAccountViewController: UITextViewDelegate {
       
     }
     return false
+  }
+}
+
+
+///TextFieldDelegate responsible for active texftfield
+extension CreateAccountViewController: UITextFieldDelegate {
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    activeTextField = textField //asigning our variable to textfield
+    activeTextField?.layer.borderWidth = 1.0
+    activeTextField?.layer.cornerRadius = 10
+    activeTextField?.layer.borderColor = UIColor.blue.cgColor
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    activeTextField?.layer.borderWidth = 0
+    activeTextField?.layer.cornerRadius = 10
+    activeTextField = nil //disabling active field
+    
   }
 }
