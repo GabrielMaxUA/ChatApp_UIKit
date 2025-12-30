a0b2754b06be9eb1c7f642a826d8a1385288246b
Max Gabriel <maxim.don.mg@gmail.com>
2025-12-29 19:09:46 -0500

    added the link functionality (attributed sting) for auth and sign in

--- ChatApp/ViewControllers/CreateAccountViewController.swift
+++ ChatApp/ViewControllers/CreateAccountViewController.swift
@@ -21,6 +21,21 @@ class CreateAccountViewController: UIViewController {
         super.viewDidLoad()
         containerView.clipsToBounds = true
         containerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+      let attributedString = NSMutableAttributedString(string: "Already have an account? Sign in here.", attributes: [
+        .font: Font.caption
+      ])
+      
+      //MARK: - adding a link functionality to a attributed string creating a range of the string we need
+      attributedString.addAttribute(.link, value: "chatappsignin://login", range: (attributedString.string as NSString).range(of:"Sign in here."))
+      signinAccountTextView.linkTextAttributes = [.foregroundColor: UIColor.secondary, .font: Font.linkFont] //applying the linkcolor to the entire with textview.linktextattributes function
+      signinAccountTextView.delegate = self
+      signinAccountTextView.isEditable = false
+      signinAccountTextView.isScrollEnabled = false
+      let paragrapghStyle = NSMutableParagraphStyle() //making sure its mutable!!!in order to work with text properties/functions
+      paragrapghStyle.alignment = .center
+      attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragrapghStyle, range: .init(location: 0, length: attributedString.length))
+      signinAccountTextView.attributedText = attributedString
+      
     }
     
     override func viewDidLayoutSubviews() {
@@ -32,4 +47,18 @@ class CreateAccountViewController: UIViewController {
     }
     
 
+}
+
+
+//MARK: - using the delegate in order to apply the link functionality to a part of a textView
+extension CreateAccountViewController: UITextViewDelegate {
+  
+  func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange) -> Bool {
+    if URL.scheme == "chatappsignin" {
+      performSegue(withIdentifier: "SignInSegue", sender: nil)
+    }
+    return false
+  }
+  
+  
 }
--- ChatApp/ViewControllers/SignInViewController.swift
+++ ChatApp/ViewControllers/SignInViewController.swift
@@ -20,6 +20,21 @@ class SignInViewController: UIViewController {
         super.viewDidLoad()
         containerView.clipsToBounds = true
         containerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+      
+      let attributedString = NSMutableAttributedString(string: "Don't have an account? Create an account here.", attributes: [
+        .font: Font.caption
+      ])
+      
+      //MARK: - adding a link functionality to a attributed string creating a range of the string we need
+      attributedString.addAttribute(.link, value: "chatappcreate://createAccount", range: (attributedString.string as NSString).range(of:"Create an account here."))
+      createAccountTextView.linkTextAttributes = [.foregroundColor: UIColor.secondary, .font: Font.linkFont] //applying the linkcolor to the entire with textview.linktextattributes function
+      createAccountTextView.delegate = self
+      createAccountTextView.isEditable = false
+      createAccountTextView.isScrollEnabled = false
+      let paragrapghStyle = NSMutableParagraphStyle() //making sure its mutable!!!in order to work with text properties/functions
+      paragrapghStyle.alignment = .center
+      attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragrapghStyle, range: .init(location: 0, length: attributedString.length))
+      createAccountTextView.attributedText = attributedString 
     }
     
     override func viewDidLayoutSubviews() {
@@ -31,4 +46,18 @@ class SignInViewController: UIViewController {
         
     }
 
+}
+
+
+//MARK: - using the delegate in order to apply the link functionality to a part of a textView
+extension SignInViewController: UITextViewDelegate {
+  
+  func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange) -> Bool {
+    if URL.scheme == "chatappcreate" {
+      performSegue(withIdentifier: "CreateAccountSegue", sender: nil)
+    }
+    return false
+  }
+  
+  
 }
